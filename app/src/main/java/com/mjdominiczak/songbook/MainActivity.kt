f4bde37e65f2fffb613413445b9bdad0f1cb397a
Michal Dominiczak <michal-dominiczak@o2.pl>
2022-02-23 20:54:01 +0100

    Theming updated to Jetpack Compose

--- app/src/main/java/com/mjdominiczak/songbook/MainActivity.kt
+++ app/src/main/java/com/mjdominiczak/songbook/MainActivity.kt
@@ -1,17 +1,29 @@
 package com.mjdominiczak.songbook
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
 import com.mjdominiczak.songbook.presentation.SongListScreen
+import com.mjdominiczak.songbook.presentation.theme.SongbookTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SongListScreen()
+            SongbookTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    SongListScreen()
+                }
+            }
         }
     }
 }
