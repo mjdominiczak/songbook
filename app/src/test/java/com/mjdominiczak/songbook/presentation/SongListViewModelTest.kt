ba05d0369ee2771597277c79337653bc693a1d4d
Michal Dominiczak <michal-dominiczak@o2.pl>
2022-03-03 23:53:03 +0100

    Added tests for SongListViewModel

---
+++ app/src/test/java/com/mjdominiczak/songbook/presentation/SongListViewModelTest.kt
@@ -0,0 +1,84 @@
+package com.mjdominiczak.songbook.presentation
+
+import com.google.common.truth.Truth.assertThat
+import com.mjdominiczak.songbook.common.Resource
+import com.mjdominiczak.songbook.data.Song
+import com.mjdominiczak.songbook.domain.GetAllSongsUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SongListViewModelTest {
+
+    @get:Rule
+    val coroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: SongListViewModel
+
+    private val getAllSongsUseCase = mock(GetAllSongsUseCase::class.java)
+
+    @Before
+    fun setUp() {
+        `when`(getAllSongsUseCase()).thenReturn(
+            flow {
+                emit(
+                    Resource.Success(
+                        listOf(
+                            Song(1, 1, "asdf", "", "", "", "", 0, emptyList()),
+                            Song(2, 1, "qwer", "", "", "", "", 0, emptyList()),
+                            Song(3, 1, "dgfg", "", "", "", "", 0, emptyList()),
+                        )
+                    )
+                )
+            }
+        )
+        viewModel = SongListViewModel(getAllSongsUseCase)
+    }
+
+    @Test
+    fun activateSearch_stateUpdatedCorrectly() {
+        viewModel.activateSearch()
+        assertThat(viewModel.state.value.isSearchActive).isTrue()
+    }
+
+    @Test
+    fun deactivateSearch_stateUpdatedCorrectly() {
+        viewModel.deactivateSearch()
+        assertThat(viewModel.state.value.isSearchActive).isFalse()
+        assertThat(viewModel.state.value.searchQuery).isEmpty()
+        viewModel.activateSearch()
+    }
+
+    @Test
+    fun onSearchQueryChanged_stateUpdatedCorrectly() {
+        viewModel.onSearchQueryChanged("test")
+        assertThat(viewModel.state.value.searchQuery).isEqualTo("test")
+    }
+
+    @Test
+    fun songsFiltered_withEmptyQuery_returnsFullList() {
+        viewModel.activateSearch()
+        viewModel.onSearchQueryChanged("")
+        assertThat(viewModel.songsFiltered).hasSize(3)
+    }
+
+    @Test
+    fun songsFiltered_withQueryMatchingMany_returnsAllMatches() {
+        viewModel.activateSearch()
+        viewModel.onSearchQueryChanged("d")
+        assertThat(viewModel.songsFiltered).hasSize(2)
+    }
+
+    @Test
+    fun songsFiltered_withQueryMatchingNothing_returnsEmptyList() {
+        viewModel.activateSearch()
+        viewModel.onSearchQueryChanged("eerfgdzfgd")
+        assertThat(viewModel.songsFiltered).isEmpty()
+    }
+
+}
