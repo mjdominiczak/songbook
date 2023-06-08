1b584e5f98adae737e4f6b0a327b59325eeda9d0
Michal Dominiczak <michal-dominiczak@o2.pl>
2023-06-08 22:12:52 +0200

    Read songs from Realm

--- app/src/main/java/com/mjdominiczak/songbook/data/SongRepositoryImpl.kt
+++ app/src/main/java/com/mjdominiczak/songbook/data/SongRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.mjdominiczak.songbook.data
 
 import com.mjdominiczak.songbook.data.local.SongDatabase
+import com.mjdominiczak.songbook.data.local.toSong
 import com.mjdominiczak.songbook.data.remote.SongApi
 import com.mjdominiczak.songbook.domain.SongRepository
 import javax.inject.Inject
@@ -9,13 +10,18 @@ class SongRepositoryImpl @Inject constructor(
     private val api: SongApi,
     private val db: SongDatabase
 ) : SongRepository {
+
+    companion object {
+        const val API_ENABLED = false
+    }
+
     override suspend fun addSong(song: Song) {
         db.addSong(song)
     }
 
-    override suspend fun getAllSongs(): List<Song> {
-        return api.getAllSongs()
-    }
+    override suspend fun getAllSongs(): List<Song> =
+        if (API_ENABLED) api.getAllSongs()
+        else db.getAllSongs().toList().map { it.toSong() }
 
     override suspend fun getSongById(id: Int): Song {
         return api.getSongById(id)
